dc07d88148d27bdfc159c796906113c07f07a031
villani <felipevillani@gmail.com>
2023-11-13 22:28:39 -0300

    init request api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import axios from "axios";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-    useEffect(() => {}, []);
+    const [theme, setTheme] = useState("saude");
 
-    return <div className="App">Teste</div>;
+    const [year, setYear] = useState(2023);
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        const signal = controller.signal;
+
+        const request = axios.get(`https://mba-api.vercel.app/city/${theme}/${year}`, {
+            signal,
+        });
+
+        request
+            .then((response) => {
+                console.log("response", response);
+                setData(response.data);
+            })
+            .catch((err) => {
+                console.error(`Erro: ${err.mensage}`);
+            });
+
+        return () => controller.abort();
+    }, [theme, year]);
+
+    return (
+        <div className="App">
+            <h1>Praia Grande</h1>
+            <div>
+                {data.map((item) => (
+                    <div key={item.city_id}>
+                        <p>{item.theme}</p>
+                        <p>{item.year}</p>
+                        <p>{item.value}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
