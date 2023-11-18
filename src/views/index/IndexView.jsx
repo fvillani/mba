53baad7c1fb441316e221ae19bc35240dd2612ac
villani <felipevillani@gmail.com>
2023-11-17 21:43:04 -0300

    commit error and loading status

--- src/views/index/IndexView.jsx
+++ src/views/index/IndexView.jsx
@@ -4,13 +4,19 @@ import { useEffect, useState } from "react";
 import "./IndexView.css";
 import IndexSearch from "./ui/IndexSearch";
 import IndexTable from "./ui/IndexTable";
+import Loading from "../../components/loading/Loading";
+import Error from "../../components/error/Error";
 
 const IndexView = () => {
-    const [theme, setTheme] = useState(null);
+    const [theme, setTheme] = useState("");
 
-    const [year, setYear] = useState(null);
+    const [year, setYear] = useState("");
 
     const [data, setData] = useState([]);
+
+    const [loading, setLoading] = useState(false);
+
+    const [error, setError] = useState("");
 
     useEffect(() => {
         const controller = new AbortController();
@@ -18,6 +24,8 @@ const IndexView = () => {
         const signal = controller.signal;
 
         if (theme !== "" && year !== "") {
+            setLoading(true);
+
             const request = axios.get(`https://mba-api.vercel.app/city/${theme}/${year}`, {
                 signal,
             });
@@ -25,9 +33,14 @@ const IndexView = () => {
             request
                 .then((response) => {
                     setData(response.data);
+                    setError("");
                 })
                 .catch((err) => {
+                    setError("Falha na requisição, tente novamente mais tarde");
                     console.error(`Erro: ${err.mensage}`);
+                })
+                .finally(() => {
+                    setLoading(false);
                 });
         } else {
             setData([]);
@@ -50,7 +63,7 @@ const IndexView = () => {
 
             <IndexSearch handleChangeTheme={handleChangeTheme} handleChangeYear={handleChangeYear} />
 
-            <IndexTable data={data} />
+            {loading ? <Loading /> : error !== "" ? <Error msg={error} /> : <IndexTable data={data} />}
         </div>
     );
 };
